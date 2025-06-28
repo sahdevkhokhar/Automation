94dd34f15a57e30d615f73694150a14c39e1a4a2
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:53:01 +0530

    Create SimpleIntre.java
    commit

---
+++ 28-6/SimpleIntre.java
@@ -0,0 +1,29 @@
+package a286;
+
+public class SimpleIntre 
+{
+
+public static void main(String[] args)
+{
+
+//I=PRN/100;
+
+      int prin = 50000;
+      int rate = 5;
+      int year = 4;
+      int intrest;
+      
+      intrest = prin*rate*year/100;
+    		  
+    		  System.out.println("You have to pay total intrest is :"+intrest);
+
+      int ans = prin+intrest;
+    		  
+    		  System.out.println("You have to pay total Amount is :"+ans);
+
+
+
+
+}
+
+}
