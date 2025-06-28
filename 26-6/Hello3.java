f2c70d3d0c37cd393dc33ca41011e4128113f934
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:56:39 +0530

    Create Hello3.java
    commit

---
+++ 26-6/Hello3.java
@@ -0,0 +1,11 @@
+
+public class Hello3 {
+//main method
+	public static void main(String args[])
+	{
+		System.out.println("\n sahdev"); 
+		System.out.println("\n G");	
+		System.out.println("\n khokhar");
+		System.out.println("Name : Sahdev \tNumber 9712717790");
+	}
+}
