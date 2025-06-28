7beb6ca947a93cb1e8055bc3e705c6b5c758fdb3
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:54:04 +0530

    Create ArathmeticopEx.java
    commit

---
+++ 28-6/ArathmeticopEx.java
@@ -0,0 +1,28 @@
+package a286;
+
+public class ArathmeticopEx 
+{
+public static void main(String[] args)
+{
+          int num1 = 4000;
+          int num2 = 2000;
+          
+          int add,sub,div,mul,mod;
+          
+          add = num1+num2;
+          sub = num1-num2;
+          mul = num1*num2;
+          div = num1/num2;
+          mod = num1%num2;
+          
+         
+          System.out.println("Addition of two value is : "+add);
+          System.out.println("Substraction of two value is : "+sub);
+          System.out.println("Multiplication of two value is : "+mul);
+          System.out.println("Division of two value is : "+div);
+          System.out.println("Modulus of two value is :" +mod);
+          
+		  
+
+}	
+}
