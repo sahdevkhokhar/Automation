1bc94f062999d3ebf9f44757f49fc86faf77248e
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:55:11 +0530

    Create AreaofTri.java
    commit

---
+++ 28-6/AreaofTri.java
@@ -0,0 +1,23 @@
+package a286;
+
+public class AreaofTri 
+{
+//0.5*a*b
+
+public static void main(String[] args)
+
+{
+float value = 0.5f,ans;
+int a=10,b=25;
+
+ans = value*a*b;
+
+System.out.println("Area of tringle is :"+ans);
+
+
+
+
+
+
+}
+}
