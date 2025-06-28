1ed21967111758f3262f4ff81dd06d73aafb7046
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:52:37 +0530

    Create SecondEx.java
    commit

---
+++ 28-6/SecondEx.java
@@ -0,0 +1,19 @@
+package a286;
+
+public class SecondEx 
+{
+public static void main(String[] args )
+{
+	int num1 = 1000;
+	int num2 = 2000;
+			
+	System.out.println("Number1 is : "+num1);
+	System.out.println("\nNumber2 is : "+num2);
+	
+	
+
+
+
+}
+
+}
