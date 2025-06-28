a129c8a1f6e53b3824a0f162234466a3211a89b3
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:47:53 +0530

    Create First.java
    commit

---
+++ 28-6/First.java
@@ -0,0 +1,16 @@
+package a286;
+
+public class First {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("\n Hello from tops Technologies");
+	System.out.println("\n Number : 9712717790 \t City:Rajkot");
+	
+	
+	
+	
+	
+	
+}
+}
