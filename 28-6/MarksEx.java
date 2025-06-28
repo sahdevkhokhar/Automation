2f4311730fc3a64282765b0cdc527d249fc30f11
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:53:42 +0530

    Create MarksEx.java
    commit

---
+++ 28-6/MarksEx.java
@@ -0,0 +1,23 @@
+package a286;
+
+public class MarksEx 
+{
+
+	public static void main(String[] args)
+	{
+		int m=85,e=80,s=75,h=70;
+		int total;
+		float per;
+			
+			total = m+e+h+s;
+		    per = total/4;
+		    		
+		    		System.out.println("My Percentage is :"+per);
+	
+	
+	
+		
+}
+}
+
+
