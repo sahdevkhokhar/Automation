e6b5d3bf23bd991b4b822804d55b4753bf12a6d8
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-06-28 14:54:25 +0530

    Create AreaofCircle.java
    commit

---
+++ 28-6/AreaofCircle.java
@@ -0,0 +1,19 @@
+package a286;
+
+public class AreaofCircle 
+{
+public static void main(String[] args)
+{
+	float PI = 3.14f,ans;
+	int r = 10;
+	ans = PI*r*r;
+	
+	System.out.println("\nArea of circle is : "+ans);
+}
+
+
+
+
+
+}
+
