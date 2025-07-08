00956916394de6c4f67d4d82f26104569d42c02a
sahdevkhokhar <sahdev.khkhr@gmail.com>
2025-07-08 10:44:32 +0530

    commit

---
+++ 7july-Task/Naturalnum1_10.java
@@ -0,0 +1,18 @@
+package July7_Task;
+
+public class Naturalnum1_10 
+{
+public static void main(String[] args) 
+
+{
+       int num = 1;
+       while(num<=10)
+       {
+    	   System.out.println(num);
+    	   
+    	   num++;
+    	    
+       }      	
+}
+		
+}
